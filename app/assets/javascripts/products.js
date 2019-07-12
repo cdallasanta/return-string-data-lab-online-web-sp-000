0ae48538b64725f81a26602c6b5a33900b701bce
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-07-12 19:54:55 +0000

    Automatically backed up by Learn

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,6 +1,7 @@
 $(function(){
   $(".js-product-more").on('click', function(){
     let prodId = $(this).data("id");
+    debugger;
     $().get(`/products/${prodId}/description`, function(data){
       debugger;
     })
