9c037403721cc0e58e3abc4ac0b9b17401f7c44d
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-07-12 20:24:26 +0000

    Automatically backed up by Learn

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -4,12 +4,12 @@ $(function(){
 
     // get product description
     $.get(`/products/${prodId}/description`, function(data){
+      debugger;
       $(`#product-${prodId}-description`).text(data);
     })
 
     // If it's sold out, say so
     $.get(`/products/${prodId}/inventory`, function(data){
-      debugger;
       if (data === "false") {
         $(`#product-${prodId}-description`).append(" - Sold out");
       } else {
