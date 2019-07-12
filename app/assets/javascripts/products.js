84339a483b62a1a00498505196815b40d4059aa5
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-07-12 20:43:23 +0000

    Automatically backed up by Learn

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -5,15 +5,14 @@ $(function(){
     // get product description
     $.get(`/products/${prodId}/description`, function(data){
       $(`#product-${prodId}-description`).text(data);
-    })
-
-    // If it's sold out, say so
-    $.get(`/products/${prodId}/inventory`, function(data){
-      if (data === "false") {
-        $(`#product-${prodId}-description`).append(" - Sold out");
-      } else {
-        $(`#product-${prodId}-description`).append(" - Available");
-      }
+      // If it's sold out, say so
+      $.get(`/products/${prodId}/inventory`, function(data){
+        if (data === "false") {
+          $(`#product-${prodId}-description`).append(" - Sold out");
+        } else {
+          $(`#product-${prodId}-description`).append(" - Available");
+        }
+      })
     })
   })
 })
